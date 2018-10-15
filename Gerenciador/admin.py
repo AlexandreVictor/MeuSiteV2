ba4158e7564c020fe0124f956cf38dcfd12c27a5
alexandrevictor <alexandrevct@hotmail.com>
2018-10-15 00:07:41 -0300

    initial commit

---
+++ Gerenciador/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import cadastro, autor
+class cadastroAdmin(admin.ModelAdmin):
+
+    model = cadastro
+
+
+    fieldsets = [
+        ('Dados Gerais', {'fields': ['fk_autor','reu']}),
+        ('Endereço da Inicial', {'fields': [(('rua','numero','bairro')),(('cidade','cep'))]}),
+        ('Dados Processo', {'fields': [(('numero_processo', 'segredo','advogado'))]}),
+
+    ]
+
+
+    list_display = ['fk_autor','numero_processo', 'bairro','cidade','segredo','advogado','data_inclusao']
+    list_filter = ['fk_autor', 'bairro', 'cidade','segredo','advogado']
+    search_fields = ['numero_processo']
+   # save_on_top = True
+
+admin.site.register(cadastro,cadastroAdmin)
+admin.site.register(autor)
---
+++ Gerenciador/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+class autor (models.Model):
+
+    autor = models.CharField(max_length=200)
+    codigo = models.IntegerField()
+
+    def __str__(self):
+        return '%s - %s' % (self.autor, self.codigo)
+
+class cadastro (models.Model):
+
+
+    fk_autor=models.ForeignKey(autor, on_delete=models.CASCADE, verbose_name ='Autor')
+    reu = models.CharField(max_length=200)
+    documento = models.CharField(max_length=14)
+    rua = models.CharField(max_length=200)
+    numero = models.IntegerField(null=True)
+    bairro = models.CharField(max_length=200)
+    cidade = models.CharField(max_length=200)
+    complemento = models.CharField(max_length=200, null=True)
+    cep = models.CharField(max_length=8)
+    numero_processo = models.IntegerField()
+    #segredo = models.IntegerField(default=0)
+    segredo = models.BooleanField(default=False ,verbose_name='É segredo ?')
+    advogado= models.BooleanField(default=False ,verbose_name='Tem advogado ?')
+    data_inclusao = models.DateTimeField(auto_now=True, verbose_name ='Incluido em')
+    fk_conta =  models.IntegerField(default=0)
+
+
+class statusgeral (models.Model):
+
+    fk_autor = models.ForeignKey(autor, on_delete=models.CASCADE)
+    fk_cadastro = models.ForeignKey(cadastro, on_delete=models.CASCADE)
+    data_alteracao = models.DateTimeField(auto_now=True)
+    status_acompanhamento = models.CharField(max_length=20)
